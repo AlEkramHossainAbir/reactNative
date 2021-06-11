f163c0ae6bbe4593e7f398fedd3361619cb73935
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2021-06-11 22:56:23 +0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,76 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+// import React, { Component } from "react";
+// import { Button, Text, View } from "react-native";
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// class App extends Component {
+//   state = { isHungry: true };
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//   render() {
+//     return (
+//       <>
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+//         <Text>
+//           I am {this.props.name}, and I am
+//           {this.state.isHungry ? " hungry" : " full"}!
+//         </Text>
+//         <Button
+//           onPress={() => {
+//             this.setState({ isHungry: false });
+//           }}
+//           disabled={!this.state.isHungry}
+//           title={
+//             this.state.isHungry ? "Pour me some milk, please!" : "Thank you!"
+//           }
+//         />
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+//       </>
+//     );
+//   }
+// }
+// export default App
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+import React, {Component} from 'react';
+import {Alert, Button, Text, View} from 'react-native';
+import Show from './pages/Show';
 
-  return (
-    <View style={styles.cont}>
-      <Text>Check</Text>
-    </View>
-  );
-};
+class App extends Component {
+  state = {ishungry: true};
+  render() {
+    return (
+      <View style={{alignItems:'center', backgroundColor:'orange', height:"100%"}}>
+        <View style={{alignItems:'flex-end',flexDirection:'row', backgroundColor:'teal'}}>
+        <Text style={{fontSize:20}}>I am {this.state.ishungry ? 'Hungry' : 'Full'}</Text>
+        <Button
+          onPress={() => {
+            this.setState({ishungry: false});
+          }}
+          disabled={!this.state.ishungry}
+          title={this.state.ishungry ? 'Give me kachi' : 'No thank you'}
+        />
+        <Show />
+      </View>
+      <Text style={{fontSize:50}}>Your App is Running !</Text>
+      <Button onPress={()=>{Alert.alert('Ore baba')}}
+      color="#55e07a"
+          title='Log in'
+        />
+      </View>
 
-const styles = StyleSheet.create({
-  
-  cont:{
-    backgroundColor:"#0000ff"
-  },
-});
+      // <View style={{flexDirection:'column'}}>
+      //   <View style={{height:'33%', width:'100%',backgroundColor:"skyblue"}}></View>
+      //   <View style={{height:'33%', width:'100%'}}></View>
+      //   <View style={{height:'35%', width:'100%',backgroundColor:"skyblue"}}></View>
+        
+      // </View>
+      
+    );
+  }
+}
 
 export default App;
